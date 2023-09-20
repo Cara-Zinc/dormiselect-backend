79bd760419c06520e50713b24d40c0c332b616f6
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2023-09-20 21:13:54 +0800

    Initial commit.

---
+++ src/main/kotlin/cs309/dormiselect/backend/DormiselectBackendApplication.kt
@@ -0,0 +1,11 @@
+package cs309.dormiselect.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DormiselectBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<DormiselectBackendApplication>(*args)
+}
