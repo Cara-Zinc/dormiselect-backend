06cef2e1b6f3d3e8ce80d076638dcaf46564bb10
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2023-11-11 20:31:51 +0800

    Fixed password checking.

--- src/main/kotlin/cs309/dormiselect/backend/domain/Account.kt
+++ src/main/kotlin/cs309/dormiselect/backend/domain/Account.kt
@@ -14,14 +14,16 @@ abstract class Account(
     @GeneratedValue
     val id: Int? = null
 
-    var password: String = "{bcrypt}${encoder.encode(password0)}"
+    var password: String = "$BCRYPT_PREFIX${encoder.encode(password0)}"
         set(value) {
-            field = "{bcrypt}${encoder.encode(value)}"
+            field = "$BCRYPT_PREFIX${encoder.encode(value)}"
         }
 
-    fun checkPassword(passwordCompared: String) = encoder.matches(passwordCompared.removePrefix("{bcrypt}"), password)
+    fun checkPassword(passwordCompared: String) =
+        encoder.matches(passwordCompared, password.removePrefix(BCRYPT_PREFIX))
 
     companion object {
         private val encoder = BCryptPasswordEncoder()
+        private const val BCRYPT_PREFIX = "{bcrypt}"
     }
 }
