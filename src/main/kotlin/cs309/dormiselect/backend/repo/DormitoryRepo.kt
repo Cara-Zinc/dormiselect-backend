553290d6c6f2f2e9fdace0e2456fa5ffa2b5a00d
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2023-11-26 21:32:15 +0800

    Helping fix bugs.

--- src/main/kotlin/cs309/dormiselect/backend/repo/DormitoryRepo.kt
+++ src/main/kotlin/cs309/dormiselect/backend/repo/DormitoryRepo.kt
@@ -1,11 +1,12 @@
 package cs309.dormiselect.backend.repo
 
 import cs309.dormiselect.backend.domain.Dormitory
-import jakarta.persistence.Id
 import org.springframework.data.repository.CrudRepository
 
-interface DormitoryRepo: CrudRepository<Dormitory, Int> {
+interface DormitoryRepo : CrudRepository<Dormitory, Int> {
     fun findByBuildingId(buildingId: String): List<Dormitory>
-    fun findById(dormitoryId: String): Dormitory?
+
+    //CrudRepository already has a function findById(id: ID): Optional<T>
+    //fun findById(dormitoryId: String): Dormitory?
     fun findBySize(size: Int): List<Dormitory>
 }
